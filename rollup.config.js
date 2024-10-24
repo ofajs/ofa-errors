19c3aa225c2dfc5118b0763976ddfad30f65ad57
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-10-24 17:56:04 +0800

    fix: rollup script

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,31 @@
 const path = require("path");
+const fs = require("fs");
 const CWD = process.cwd();
 const PACKAGE = require(path.join(CWD, "package.json"));
 const terser = require("@rollup/plugin-terser");
-const json = require("@rollup/plugin-json");
 
 const banner = `//! ${PACKAGE.name} - v${PACKAGE.version} ${
   PACKAGE.homepage
 }  (c) ${PACKAGE.startyear}-${new Date().getFullYear()} ${PACKAGE.author.name}`;
 
-module.exports = [
-  {
-    input: "src/ofa-error-zh.js",
-    output: [
-      {
-        file: "dist/ofa-error-zh.js",
-        format: "umd",
-        name: "$",
-        banner,
-      },
-    ],
-    plugins: [json()],
-  },
-];
+const finnalOpts = fs
+  .readdirSync("src")
+  .filter((file) => file.endsWith(".js"))
+  .map((file) => {
+    return {
+      input: path.join("src", file),
+      output: [
+        {
+          dir: "dist/",
+          format: "umd",
+          name: "$",
+          banner,
+        },
+      ],
+      plugins: [terser()],
+    };
+  });
+
+console.log(finnalOpts);
+
+module.exports = finnalOpts;
